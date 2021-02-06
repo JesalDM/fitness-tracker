f9f5fbd98b27cdb6f166273e48419c00ac4b6b7d
Jesal Mehta <jesaldmehta@gmail.com>
2021-02-06 17:48:42 -0500

    Removing the reference to seed.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const db = require("./models");
 require('dotenv').config();
-require("./seeders/seed.js");
 
 // Sets up the Express App
 const app = express();
